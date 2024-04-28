d1554716bf7f694ba77a68445847d89bfc9e42aa
Yesenia Guadalupe Castro Salazar <yesenigupecastros@gmail.com>
2024-04-27 20:26:24 -0700

    se cambio el color en el datdrigview de gestor mascotas,y gestor adoptantes, tambien se coloco un label al seleccionar el reporte en el general formato de adopcion

--- AMAC/Views/FormatManagement/FormatUpdateView/FormatUpdateView.Designer.cs
+++ AMAC/Views/FormatManagement/FormatUpdateView/FormatUpdateView.Designer.cs
@@ -44,6 +44,7 @@
             this.panelSubTab = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
             this.cbPdfFormats = new DevExpress.XtraEditors.LookUpEdit();
+            this.label1 = new System.Windows.Forms.Label();
             this.tableLayoutPanel2.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -64,22 +65,22 @@
             this.tableLayoutPanel2.Controls.Add(this.panel2, 1, 0);
             this.tableLayoutPanel2.Controls.Add(this.panel1, 0, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(4, 27);
-            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 35);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(1261, 40);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1082, 37);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
             // panel3
             // 
             this.panel3.Controls.Add(this.btnVolunter);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(844, 2);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.panel3.Location = new System.Drawing.Point(723, 2);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(413, 36);
+            this.panel3.Size = new System.Drawing.Size(356, 33);
             this.panel3.TabIndex = 2;
             // 
             // btnVolunter
@@ -94,9 +95,9 @@
             this.btnVolunter.Enabled = false;
             this.btnVolunter.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnVolunter.ImageOptions.Image")));
             this.btnVolunter.Location = new System.Drawing.Point(0, 0);
-            this.btnVolunter.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.btnVolunter.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnVolunter.Name = "btnVolunter";
-            this.btnVolunter.Size = new System.Drawing.Size(413, 36);
+            this.btnVolunter.Size = new System.Drawing.Size(356, 33);
             this.btnVolunter.TabIndex = 3;
             this.btnVolunter.Tag = "Responsability";
             this.btnVolunter.Text = "Responsabilidades";
@@ -105,10 +106,10 @@
             // 
             this.panel2.Controls.Add(this.btnAdopter);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(424, 2);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.panel2.Location = new System.Drawing.Point(363, 2);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(412, 36);
+            this.panel2.Size = new System.Drawing.Size(354, 33);
             this.panel2.TabIndex = 1;
             // 
             // btnAdopter
@@ -123,9 +124,9 @@
             this.btnAdopter.Enabled = false;
             this.btnAdopter.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnAdopter.ImageOptions.Image")));
             this.btnAdopter.Location = new System.Drawing.Point(0, 0);
-            this.btnAdopter.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.btnAdopter.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAdopter.Name = "btnAdopter";
-            this.btnAdopter.Size = new System.Drawing.Size(412, 36);
+            this.btnAdopter.Size = new System.Drawing.Size(354, 33);
             this.btnAdopter.TabIndex = 2;
             this.btnAdopter.Tag = "Adopter";
             this.btnAdopter.Text = "Datos del Adoptante";
@@ -134,10 +135,10 @@
             // 
             this.panel1.Controls.Add(this.btnAnimal);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(4, 2);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.panel1.Location = new System.Drawing.Point(3, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(412, 36);
+            this.panel1.Size = new System.Drawing.Size(354, 33);
             this.panel1.TabIndex = 0;
             // 
             // btnAnimal
@@ -152,9 +153,9 @@
             this.btnAnimal.Enabled = false;
             this.btnAnimal.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnAnimal.ImageOptions.Image")));
             this.btnAnimal.Location = new System.Drawing.Point(0, 0);
-            this.btnAnimal.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.btnAnimal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAnimal.Name = "btnAnimal";
-            this.btnAnimal.Size = new System.Drawing.Size(412, 36);
+            this.btnAnimal.Size = new System.Drawing.Size(354, 33);
             this.btnAnimal.TabIndex = 1;
             this.btnAnimal.Tag = "Animal";
             this.btnAnimal.Text = "Datos de la Mascota";
@@ -169,15 +170,15 @@
             this.tableLayoutPanel1.Controls.Add(this.panel4, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35.78947F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 64.21053F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 448F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 175F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1269, 693);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 44.59459F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 55.40541F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 346F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 142F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 16F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1088, 563);
             this.tableLayoutPanel1.TabIndex = 3;
             // 
             // panel5
@@ -186,10 +187,10 @@
             this.panel5.Controls.Add(this.btnSave);
             this.panel5.Controls.Add(this.label10);
             this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel5.Location = new System.Drawing.Point(4, 519);
-            this.panel5.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.panel5.Location = new System.Drawing.Point(3, 422);
+            this.panel5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(1261, 172);
+            this.panel5.Size = new System.Drawing.Size(1082, 139);
             this.panel5.TabIndex = 7;
             // 
             // btnDelete
@@ -201,10 +202,9 @@
             this.btnDelete.ForeColor = System.Drawing.Color.White;
             this.btnDelete.Image = ((System.Drawing.Image)(resources.GetObject("btnDelete.Image")));
             this.btnDelete.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnDelete.Location = new System.Drawing.Point(19, 32);
-            this.btnDelete.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDelete.Location = new System.Drawing.Point(16, 26);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(184, 68);
+            this.btnDelete.Size = new System.Drawing.Size(158, 55);
             this.btnDelete.TabIndex = 309;
             this.btnDelete.Text = "        ELIMINAR";
             this.btnDelete.UseVisualStyleBackColor = false;
@@ -218,10 +218,9 @@
             this.btnSave.ForeColor = System.Drawing.Color.White;
             this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
             this.btnSave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSave.Location = new System.Drawing.Point(211, 32);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSave.Location = new System.Drawing.Point(181, 26);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(184, 68);
+            this.btnSave.Size = new System.Drawing.Size(158, 55);
             this.btnSave.TabIndex = 306;
             this.btnSave.Text = "GUARDAR CAMBIOS";
             this.btnSave.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -232,10 +231,9 @@
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
             this.label10.ForeColor = System.Drawing.Color.Red;
-            this.label10.Location = new System.Drawing.Point(429, 55);
-            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Location = new System.Drawing.Point(368, 45);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(138, 23);
+            this.label10.Size = new System.Drawing.Size(113, 18);
             this.label10.TabIndex = 305;
             this.label10.Text = "label AVISOS";
             this.label10.Visible = false;
@@ -243,47 +241,61 @@
             // panelSubTab
             // 
             this.panelSubTab.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelSubTab.Location = new System.Drawing.Point(4, 71);
-            this.panelSubTab.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.panelSubTab.Location = new System.Drawing.Point(3, 76);
+            this.panelSubTab.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panelSubTab.Name = "panelSubTab";
-            this.panelSubTab.Size = new System.Drawing.Size(1261, 444);
+            this.panelSubTab.Size = new System.Drawing.Size(1082, 342);
             this.panelSubTab.TabIndex = 4;
             // 
             // panel4
             // 
+            this.panel4.Controls.Add(this.label1);
             this.panel4.Controls.Add(this.cbPdfFormats);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel4.Location = new System.Drawing.Point(4, 2);
-            this.panel4.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.panel4.Location = new System.Drawing.Point(3, 2);
+            this.panel4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(1261, 21);
+            this.panel4.Size = new System.Drawing.Size(1082, 29);
             this.panel4.TabIndex = 5;
             // 
             // cbPdfFormats
             // 
-            this.cbPdfFormats.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.cbPdfFormats.Location = new System.Drawing.Point(0, 0);
-            this.cbPdfFormats.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.cbPdfFormats.Dock = System.Windows.Forms.DockStyle.Right;
+            this.cbPdfFormats.Location = new System.Drawing.Point(187, 0);
+            this.cbPdfFormats.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbPdfFormats.Name = "cbPdfFormats";
+            this.cbPdfFormats.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbPdfFormats.Properties.Appearance.Options.UseFont = true;
             this.cbPdfFormats.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
             this.cbPdfFormats.Properties.Columns.AddRange(new DevExpress.XtraEditors.Controls.LookUpColumnInfo[] {
-            new DevExpress.XtraEditors.Controls.LookUpColumnInfo("volunteer", "Voluntario"),
-            new DevExpress.XtraEditors.Controls.LookUpColumnInfo("animalName", "Mascota"),
-            new DevExpress.XtraEditors.Controls.LookUpColumnInfo("adopterName", "Adoptante"),
-            new DevExpress.XtraEditors.Controls.LookUpColumnInfo("adoptionDate", "Fecha de adopcion")});
+            new DevExpress.XtraEditors.Controls.LookUpColumnInfo("volunteer", "Voluntario", 17, DevExpress.Utils.FormatType.None, "", true, DevExpress.Utils.HorzAlignment.Default, DevExpress.Data.ColumnSortOrder.None, DevExpress.Utils.DefaultBoolean.Default),
+            new DevExpress.XtraEditors.Controls.LookUpColumnInfo("animalName", "Mascota", 17, DevExpress.Utils.FormatType.None, "", true, DevExpress.Utils.HorzAlignment.Default, DevExpress.Data.ColumnSortOrder.None, DevExpress.Utils.DefaultBoolean.Default),
+            new DevExpress.XtraEditors.Controls.LookUpColumnInfo("adopterName", "Adoptante", 17, DevExpress.Utils.FormatType.None, "", true, DevExpress.Utils.HorzAlignment.Default, DevExpress.Data.ColumnSortOrder.None, DevExpress.Utils.DefaultBoolean.Default),
+            new DevExpress.XtraEditors.Controls.LookUpColumnInfo("adoptionDate", "Fecha de adopcion", 17, DevExpress.Utils.FormatType.None, "", true, DevExpress.Utils.HorzAlignment.Default, DevExpress.Data.ColumnSortOrder.None, DevExpress.Utils.DefaultBoolean.Default)});
             this.cbPdfFormats.Properties.NullText = "";
-            this.cbPdfFormats.Size = new System.Drawing.Size(1261, 22);
+            this.cbPdfFormats.Size = new System.Drawing.Size(895, 26);
             this.cbPdfFormats.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(9, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Seleccione el reporte : ";
             // 
             // FormatUpdateView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1269, 693);
+            this.ClientSize = new System.Drawing.Size(1088, 563);
             this.Controls.Add(this.tableLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormatUpdateView";
             this.Text = "ModificarDocumento";
             this.tableLayoutPanel2.ResumeLayout(false);
@@ -294,6 +306,7 @@
             this.panel5.ResumeLayout(false);
             this.panel5.PerformLayout();
             this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.cbPdfFormats.Properties)).EndInit();
             this.ResumeLayout(false);
 
@@ -316,5 +329,6 @@
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label1;
     }
 }
